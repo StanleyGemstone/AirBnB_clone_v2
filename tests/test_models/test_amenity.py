2302f54945ea7365100bdec0f8fc2278b7056299
Ayom04 <abdullahayomide04@gmail.com>
2023-06-17 23:00:37 +0100

    generated test cases

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,10 +2,11 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
+import os
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """ amenity test class"""
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -14,6 +15,8 @@ class test_Amenity(test_basemodel):
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """testing name type """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(type(new.name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
